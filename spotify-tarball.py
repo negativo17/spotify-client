92d1a12b0d86163377068bd9473feb4021c0c2bf
Simone Caronni <negativo17@gmail.com>
2023-12-11 18:34:39 +0100

    Format python script

--- spotify-tarball.py
+++ spotify-tarball.py
@@ -13,40 +13,41 @@ import sys
 import tarfile
 from urllib.request import Request, urlopen
 
+
 def main():
-    repo_url = 'http://api.snapcraft.io/v2/snaps/info/spotify'
+    repo_url = "http://api.snapcraft.io/v2/snaps/info/spotify"
 
     request = Request(repo_url)
-    request.add_header('Snap-Device-Series', '16')
+    request.add_header("Snap-Device-Series", "16")
     response = urlopen(request).read()
     repo_url_raw = json.loads(response)
 
-#    print(repo_url_raw['channel-map'][2]['version'])
+    # print(repo_url_raw['channel-map'][2]['version'])
 
-    for channel_map in repo_url_raw['channel-map']:
-        if 'edge' in channel_map['channel']['name']:
-            version = channel_map['version']
-            snap_url = channel_map['download']['url']
+    for channel_map in repo_url_raw["channel-map"]:
+        if "edge" in channel_map["channel"]["name"]:
+            version = channel_map["version"]
+            snap_url = channel_map["download"]["url"]
 
-    with open('spotify-client.spec', 'r') as file:
+    with open("spotify-client.spec", "r") as file:
         for line in file:
-            if re.search('^Version:.*' + version, line, re.I):
+            if re.search("^Version:.*" + version, line, re.I):
                 print("SPEC file already contains the latest version: " + version + ".")
                 sys.exit(0)
 
     print("New version available: " + version + " (" + snap_url + ")")
 
-    print("Updating SPEC file...", end = " ")
-    rpmdev_bumpspec_comment = ("Update to version " + version + ".")
-    subprocess.run(["rpmdev-bumpspec", "-D", "-c", rpmdev_bumpspec_comment, "-n", version, "spotify-client.spec" ])
+    print("Updating SPEC file...", end=" ")
+    rpmdev_bumpspec_comment = "Update to version " + version + "."
+    subprocess.run(["rpmdev-bumpspec", "-D", "-c", rpmdev_bumpspec_comment, "-n", version, "spotify-client.spec"])
     print("done.")
 
-    tarball = ("spotify-" + version)
-    
+    tarball = "spotify-" + version
+
     print("Downloading snap file " + tarball + ".snap" + "...", end=" ", flush=True)
     request = Request(snap_url)
     response = urlopen(request).read()
-    open(tarball + ".snap", 'wb').write(response)
+    open(tarball + ".snap", "wb").write(response)
     print("done.")
 
     print("Unpacking " + tarball + ".snap" + "...", end=" ", flush=True)
@@ -63,6 +64,7 @@ def main():
     tar.close()
     shutil.rmtree(tarball)
     print("done.")
+
 
 if __name__ == "__main__":
     main()
